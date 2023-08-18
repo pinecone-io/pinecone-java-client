5706a1a4a4d8c1f1b684ac2f33e547ddb34a3aa7
rohanshah18 <rohan.s@pinecone.io>
2023-08-18 13:35:51 -0400

    add create index to java sdk client

--- src/test/java/io/pinecone/PineconeIndexOperationClientTest.java
+++ src/test/java/io/pinecone/PineconeIndexOperationClientTest.java
@@ -12,7 +12,7 @@ public class PineconeIndexOperationClientTest {
 
     @SuppressWarnings("unchecked")
     @Test
-    public void testSuccessfulIndexDeletion() throws IOException {
+    public void testIndexDeletion() throws IOException {
         AsyncHttpClient mockClient = mock(DefaultAsyncHttpClient.class);
         PineconeClientConfig clientConfig = new PineconeClientConfig().withApiKey("testApiKey").withEnvironment("testEnvironment");
         Response mockResponse = mock(Response.class);
@@ -23,7 +23,6 @@ public class PineconeIndexOperationClientTest {
         when(mockResponse.getStatusCode()).thenReturn(202);
         when(mockClient.prepare(anyString(), anyString())).thenReturn(mockBoundRequestBuilder);
         when(mockBoundRequestBuilder.setHeader(anyString(), anyString())).thenReturn(mockBoundRequestBuilder);
-        when(mockBoundRequestBuilder.setHeader(eq("Api-Key"), eq("testApiKey"))).thenReturn(mockBoundRequestBuilder);
         when(mockBoundRequestBuilder.execute()).thenReturn(mockResponseFuture);
         when(mockResponseFuture.toCompletableFuture()).thenReturn(mockCompletableFuture);
 
@@ -32,6 +31,33 @@ public class PineconeIndexOperationClientTest {
 
         verify(mockClient, times(1)).prepare(eq("DELETE"), anyString());
         verify(mockBoundRequestBuilder).setHeader(eq("Api-Key"), eq("testApiKey"));
+        verify(mockBoundRequestBuilder).execute();
+        verify(mockClient).close();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testIndexCreation() throws IOException {
+        AsyncHttpClient mockClient = mock(DefaultAsyncHttpClient.class);
+        PineconeClientConfig clientConfig = new PineconeClientConfig().withApiKey("testApiKey").withEnvironment("testEnvironment");
+        Response mockResponse = mock(Response.class);
+        BoundRequestBuilder mockBoundRequestBuilder = mock(BoundRequestBuilder.class);
+        ListenableFuture<Response> mockResponseFuture = mock(ListenableFuture.class);
+        CompletableFuture<Response> mockCompletableFuture = mock(CompletableFuture.class);
+
+        when(mockResponse.getStatusCode()).thenReturn(201);
+        when(mockClient.prepare(anyString(), anyString())).thenReturn(mockBoundRequestBuilder);
+        when(mockBoundRequestBuilder.setHeader(anyString(), anyString())).thenReturn(mockBoundRequestBuilder);
+        when(mockBoundRequestBuilder.setBody(anyString())).thenReturn(mockBoundRequestBuilder);
+        when(mockBoundRequestBuilder.execute()).thenReturn(mockResponseFuture);
+        when(mockResponseFuture.toCompletableFuture()).thenReturn(mockCompletableFuture);
+
+        PineconeIndexOperationClient indexDeletionService = new PineconeIndexOperationClient(clientConfig, mockClient);
+        indexDeletionService.createIndex("testEnvironment", 128, "cosine");
+
+        verify(mockClient, times(1)).prepare(eq("POST"), anyString());
+        verify(mockBoundRequestBuilder).setHeader(eq("accept"), eq("text/plain"));
+        verify(mockBoundRequestBuilder).setHeader(eq("content-type"), eq("application/json"));
         verify(mockBoundRequestBuilder).execute();
         verify(mockClient).close();
     }
