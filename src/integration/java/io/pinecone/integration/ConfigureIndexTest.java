a858d8a9c0d41af1202c3e24d22a4790b3405be5
rohanshah18 <rohan.s@pinecone.io>
2023-10-31 16:57:42 -0400

    add a loop to check if index status is ready

--- src/integration/java/io/pinecone/integration/ConfigureIndexTest.java
+++ src/integration/java/io/pinecone/integration/ConfigureIndexTest.java
@@ -3,6 +3,7 @@ package io.pinecone.integration;
 import io.pinecone.PineconeClientConfig;
 import io.pinecone.PineconeIndexOperationClient;
 import io.pinecone.exceptions.PineconeBadRequestException;
+import io.pinecone.exceptions.PineconeNotFoundException;
 import io.pinecone.helpers.RandomStringBuilder;
 import io.pinecone.model.ConfigureIndexRequest;
 import io.pinecone.model.CreateIndexRequest;
@@ -11,18 +12,19 @@ import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConfigureIndexTest {
     private PineconeIndexOperationClient pinecone;
-    private String indexName = RandomStringBuilder.build("index-name", 8);
+    private String indexName;
 
     @BeforeEach
-    public void setUp() throws IOException{
+    public void setUp() throws IOException {
+        indexName = RandomStringBuilder.build("index-name", 8);
         PineconeClientConfig config = new PineconeClientConfig()
                 .withApiKey(System.getenv("PINECONE_API_KEY"))
-                .withEnvironment(System.getenv("PINECONE_ENVIRONMENT"))
-                .withServerSideTimeoutSec(10);
+                .withEnvironment(System.getenv("PINECONE_ENVIRONMENT"));
         pinecone = new PineconeIndexOperationClient(config);
 
         // Create an index
@@ -38,113 +40,144 @@ public class ConfigureIndexTest {
         pinecone.deleteIndex(indexName);
     }
 
-    @Nested
-    class scalingReplicas {
-        @Test
-        public void scaleUp() throws IOException {
-            // Verify the starting state
-            IndexMeta indexMeta = pinecone.describeIndex(indexName);
-            assertEquals(1, indexMeta.getDatabase().getReplicas());
+    @Test
+    public void configureIndexWithInvalidIndexName() throws IOException, InterruptedException {
+        // Verify that configuring an invalid index name throws an exception
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withReplicas(2);
+        checkIndexStatusReady(indexName);
+        assertThrows(PineconeNotFoundException.class, () -> {
+            pinecone.configureIndex("non-existent-index", configureIndexRequest);
+        });
+    }
 
-            // Configure the index
-            ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
-                    .withReplicas(2);
+    @Test
+    public void configureIndexExceedingQuota() throws IOException, InterruptedException {
+        // Verify that configuring an index with replicas exceeding the quota throws an exception
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withReplicas(20);
+        checkIndexStatusReady(indexName);
+        assertThrows(PineconeBadRequestException.class, () ->
+            pinecone.configureIndex(indexName, configureIndexRequest));
+    }
+
+    @Test
+    public void scaleUp() throws IOException, InterruptedException {
+        // Verify the starting state
+        IndexMeta indexMeta = pinecone.describeIndex(indexName);
+        assertEquals(1, indexMeta.getDatabase().getReplicas());
+
+        // Configure the index
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withReplicas(2);
+        checkIndexStatusReady(indexName);
+        pinecone.configureIndex(indexName, configureIndexRequest);
+
+        // Verify replicas were scaled up
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals(2, indexMeta.getDatabase().getReplicas());
+    }
+
+    @Test
+    public void scaleDown() throws IOException, InterruptedException {
+        // Verify the starting state
+        IndexMeta indexMeta = pinecone.describeIndex(indexName);
+        assertEquals(1, indexMeta.getDatabase().getReplicas());
+
+        // Scale up for the test
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withReplicas(3);
+        checkIndexStatusReady(indexName);
+        pinecone.configureIndex(indexName, configureIndexRequest);
+
+        // Verify the scaled up replicas
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals(3, indexMeta.getDatabase().getReplicas());
+
+        // Scaling down
+        configureIndexRequest = new ConfigureIndexRequest()
+                .withReplicas(1);
+        checkIndexStatusReady(indexName);
+        pinecone.configureIndex(indexName, configureIndexRequest);
+
+        // Verify replicas were scaled down
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals(1, indexMeta.getDatabase().getReplicas());
+    }
+
+    @Test
+    public void changingBasePodType() throws IOException, InterruptedException {
+        // Verify the starting state
+        IndexMeta indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
+
+        // Try to change the base pod type
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withPodType("p2.x1");
+        try {
+            checkIndexStatusReady(indexName);
             pinecone.configureIndex(indexName, configureIndexRequest);
-
-            // Verify replicas were scaled up
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals(2, indexMeta.getDatabase().getReplicas());
-        }
-
-        @Test
-        public void scaleDown() throws IOException {
-            // Verify the starting state
-            IndexMeta indexMeta = pinecone.describeIndex(indexName);
-            assertEquals(1, indexMeta.getDatabase().getReplicas());
-
-            // Scale up for the test
-            ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
-                    .withReplicas(3);
-            pinecone.configureIndex(indexName, configureIndexRequest);
-
-            // Verify the scaled up replicas
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals(3, indexMeta.getDatabase().getReplicas());
-
-            // Scaling down
-            configureIndexRequest = new ConfigureIndexRequest()
-                    .withReplicas(2);
-            pinecone.configureIndex(indexName, configureIndexRequest);
-
-            // Verify replicas were scaled down
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals(2, indexMeta.getDatabase().getReplicas());
+        } catch (PineconeBadRequestException pineconeBadRequestException) {
+            assertEquals(pineconeBadRequestException.getMessage(), "updating base pod type is not supported");
         }
     }
 
-    @Nested
-    class ScalingPodTypeTests {
-        @Test
-        public void changingBasePodType() throws IOException {
-            // Verify the starting state
-            IndexMeta indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
+    @Test
+    public void sizeIncrease() throws IOException, InterruptedException {
+        // Verify the starting state
+        IndexMeta indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
 
-            // Try to change the base pod type
-            ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
-                    .withPodType("p2.x1");
-            try {
-                pinecone.configureIndex(indexName, configureIndexRequest);
-            } catch (Exception exception) {
-                assertEquals(exception.getClass(), PineconeBadRequestException.class);
-                assertEquals(exception.getMessage(), "updating base pod type is not supported");
-            }
+        // Change the pod type to a larger one
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withPodType("p1.x2");
+        checkIndexStatusReady(indexName);
+        pinecone.configureIndex(indexName, configureIndexRequest);
+
+        // Get the index description to verify the new pod type
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
+    }
+
+    @Test
+    public void sizeDown() throws IOException, InterruptedException {
+        // Verify the starting state
+        IndexMeta indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
+
+        // Increase the pod type
+        ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
+                .withPodType("p1.x2");
+        checkIndexStatusReady(indexName);
+        pinecone.configureIndex(indexName, configureIndexRequest);
+
+        // Get the index description to verify the new pod type
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
+
+        // Attempt to scale down
+        configureIndexRequest = new ConfigureIndexRequest()
+                .withPodType("p1.x1");
+        try {
+            pinecone.configureIndex(indexName, configureIndexRequest);
+        } catch (Exception exception) {
+            assertEquals(exception.getClass(), PineconeBadRequestException.class);
+            assertEquals(exception.getMessage(), "scaling down pod type is not supported");
         }
 
-        @Test
-        public void sizeIncrease() throws IOException {
-            // Verify the starting state
+        // Get the index description to verify the pod type remains unchanged
+        indexMeta = pinecone.describeIndex(indexName);
+        assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
+    }
+
+    private void checkIndexStatusReady(String indexName) throws IOException, InterruptedException {
+        while (true) {
             IndexMeta indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
-
-            // Change the pod type to a larger one
-            ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
-                    .withPodType("p1.x2");
-            pinecone.configureIndex(indexName, configureIndexRequest);
-
-            // Get the index description to verify the new pod type
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
-        }
-
-        @Test
-        public void sizeDown() throws IOException {
-            // Verify the starting state
-            IndexMeta indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x1", indexMeta.getDatabase().getPodType());
-
-            // Increase the pod type
-            ConfigureIndexRequest configureIndexRequest = new ConfigureIndexRequest()
-                    .withPodType("p1.x2");
-            pinecone.configureIndex(indexName, configureIndexRequest);
-
-            // Get the index description to verify the new pod type
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
-
-            // Attempt to scale down
-            configureIndexRequest = new ConfigureIndexRequest()
-                    .withPodType("p1.x1");
-            try {
-                pinecone.configureIndex(indexName, configureIndexRequest);
-            } catch (Exception exception) {
-                assertEquals(exception.getClass(), PineconeBadRequestException.class);
-                assertEquals(exception.getMessage(), "scaling down pod type is not supported");
+            if (indexMeta.getStatus().getState().equalsIgnoreCase("ready")) {
+                break;
             }
 
-            // Get the index description to verify the pod type remains unchanged
-            indexMeta = pinecone.describeIndex(indexName);
-            assertEquals("p1.x2", indexMeta.getDatabase().getPodType());
+            Thread.sleep(1000);
         }
     }
 }
