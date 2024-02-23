a010d1543895680599a52473b38574aacfded430
rohanshah18 <rohan.s@pinecone.io>
2024-02-23 14:27:51 -0500

    add query()

--- src/main/java/io/pinecone/PineconeDataPlaneClient.java
+++ src/main/java/io/pinecone/PineconeDataPlaneClient.java
@@ -18,7 +18,7 @@ public class PineconeDataPlaneClient {
         UpsertRequest.Builder upsertRequest = UpsertRequest.newBuilder();
 
         if (vectors.isEmpty()) {
-            throw new PineconeValidationException("");
+            throw new PineconeValidationException("Invalid Update Request. Please ensure that vectors are provided and not empty.");
         }
 
         upsertRequest.addAllVectors(vectors);
@@ -30,15 +30,45 @@ public class PineconeDataPlaneClient {
         return blockingStub.upsert(upsertRequest.build());
     }
 
-    public QueryResponse query(QueryRequest queryRequest) {
-        return blockingStub.query(queryRequest);
+    public QueryResponse query(String namespace, int topK, Struct filter, boolean includeValues, boolean includeMetadata,
+                               List<Float> vectors, SparseValues sparseVector, String id) {
+        QueryRequest.Builder queryRequest = QueryRequest.newBuilder()
+                .setTopK(topK)
+                .setIncludeValues(includeValues)
+                .setIncludeMetadata(includeMetadata);
+
+        if (id != null && !vectors.isEmpty()) {
+            throw new PineconeValidationException("Invalid Query Request. Please include only one of the following parameters: id or vector");
+        }
+
+        if (namespace != null) {
+            queryRequest.setNamespace(namespace);
+        }
+
+        if (filter != null) {
+            queryRequest.setFilter(filter);
+        }
+
+        if (!vectors.isEmpty()) {
+            queryRequest.addAllVector(vectors);
+        }
+
+        if (sparseVector != null) {
+            queryRequest.setSparseVector(sparseVector);
+        }
+
+        if (id != null) {
+            queryRequest.setId(id);
+        }
+
+        return blockingStub.query(queryRequest.build());
     }
 
     public FetchResponse fetch(Iterable<String> ids, String namespace) {
         FetchRequest.Builder fetchRequest = FetchRequest.newBuilder();
 
         if (ids == null) {
-            throw new PineconeValidationException("Vector IDs to fetch must be present");
+            throw new PineconeValidationException("Invalid Fetch Request. Vector IDs must be present");
         }
 
         fetchRequest.addAllIds(ids);
@@ -54,7 +84,7 @@ public class PineconeDataPlaneClient {
         UpdateRequest.Builder updateRequest = UpdateRequest.newBuilder();
 
         if (id == null) {
-            throw new PineconeValidationException("Vector ID must be present");
+            throw new PineconeValidationException("Invalid Update Request. Vector ID must be present");
         }
         updateRequest.setId(id);
 
@@ -73,7 +103,7 @@ public class PineconeDataPlaneClient {
         // ToDo: Update this to longToInt()
         if (sparseValues != null) {
             if (sparseValues.getValuesCount() != sparseValues.getIndicesCount()) {
-                throw new PineconeValidationException("Sparse indices and values must be of the same length.");
+                throw new PineconeValidationException("Invalid Update Request. Sparse indices and values must be of the same length.");
             }
             updateRequest.setSparseValues(sparseValues);
         }
@@ -84,11 +114,9 @@ public class PineconeDataPlaneClient {
     public DeleteResponse delete(Iterable<String> ids, boolean deleteAll, String namespace, Struct filter) {
         DeleteRequest.Builder deleteRequestBuilder = DeleteRequest.newBuilder().setDeleteAll(deleteAll);
 
-        // Confirm: add validation for empty ids?
-        if (ids == null) {
-            throw new PineconeValidationException("Vector IDs to delete must be present");
+        if (ids != null) {
+            deleteRequestBuilder.addAllIds(ids);
         }
-        deleteRequestBuilder.addAllIds(ids);
 
         if (namespace != null) {
             deleteRequestBuilder.setNamespace(namespace);
