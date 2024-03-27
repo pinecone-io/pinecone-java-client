aaaea0aa47f43c00631d3809907f31dccc83b19d
Austin DeNoble <austin.d@pinecone.io>
2024-03-27 11:30:02 -0400

    add polling to ConfigureIndexTest to ensure we are not deleting or operating on currently upgrading indexes

--- src/integration/java/io/pinecone/integration/controlPlane/pod/ConfigureIndexTest.java
+++ src/integration/java/io/pinecone/integration/controlPlane/pod/ConfigureIndexTest.java
@@ -96,6 +96,7 @@ public class ConfigureIndexTest {
             assertNotNull(podSpec);
             assertEquals(podSpec.getReplicas(), 1);
         });
+        waitUntilIndexUpgraded(indexName);
     }
 
     @Test
@@ -137,5 +138,23 @@ public class ConfigureIndexTest {
             assertNotNull(podSpec);
             assertEquals(podSpec.getPodType(), "p1.x2");
         });
+        waitUntilIndexUpgraded(indexName);
+    }
+
+    // After calling configureIndex the index needs to be upgraded which
+    // can take some time. Poll for a bit until we're ready to continue operating on the index.
+    private void waitUntilIndexUpgraded(String indexName) throws InterruptedException {
+        int timeToWaitMs = 30000;
+        IndexModel index = controlPlaneClient.describeIndex(indexName);
+
+        while (index.getStatus().getState() != IndexModelStatus.StateEnum.READY || timeToWaitMs > 0) {
+            Thread.sleep(2000);
+            timeToWaitMs -= 2000;
+            index = controlPlaneClient.describeIndex(indexName);
+        }
+
+        if (!index.getStatus().getReady()) {
+            fail("Index " + indexName + " did not finish upgrading after " + timeToWaitMs + "ms");
+        }
     }
 }
