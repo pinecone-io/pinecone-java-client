3d4ae30ad7f5d66fa8ad411b091307c2a083564f
aulorbe <aulorbe@gmail.com>
2024-04-09 18:13:31 -0700

    Updating /pod/CreateDescribeListAndDeleteIndexTest for deprecation of createIndex

--- src/integration/java/io/pinecone/integration/controlPlane/pod/CreateDescribeListAndDeleteIndexTest.java
+++ src/integration/java/io/pinecone/integration/controlPlane/pod/CreateDescribeListAndDeleteIndexTest.java
@@ -2,8 +2,6 @@ package io.pinecone.integration.controlPlane.pod;
 
 import io.pinecone.clients.Pinecone;
 import io.pinecone.exceptions.PineconeBadRequestException;
-import io.pinecone.exceptions.PineconeUnmappedHttpException;
-import io.pinecone.exceptions.PineconeValidationException;
 import io.pinecone.helpers.RandomStringBuilder;
 import io.pinecone.helpers.TestIndexResourcesManager;
 import org.junit.jupiter.api.BeforeAll;
@@ -47,29 +45,6 @@ public class CreateDescribeListAndDeleteIndexTest {
     }
 
     @Test
-    public void createIndexWithPodsAndPodType() {
-        String podIndexName = RandomStringBuilder.build("create-pod", 8);
-        String podType = "p1.x2";
-        CreateIndexRequestSpecPod podSpec = new CreateIndexRequestSpecPod().environment(environment).pods(2).podType(podType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name(podIndexName)
-                .dimension(10)
-                .metric(IndexMetric.EUCLIDEAN)
-                .spec(createIndexRequestSpec);
-
-        IndexModel createdIndex = controlPlaneClient.createIndex(createIndexRequest);
-        assertEquals(createdIndex.getName(), podIndexName);
-        assertEquals(createdIndex.getMetric(), IndexMetric.EUCLIDEAN);
-        assertEquals(createdIndex.getSpec().getPod().getPods(), 2);
-        assertEquals(createdIndex.getSpec().getPod().getPodType(), podType);
-        assertEquals(createdIndex.getStatus().getReady(), false);
-        assertEquals(createdIndex.getStatus().getState(), IndexModelStatus.StateEnum.INITIALIZING);
-
-        controlPlaneClient.deleteIndex(podIndexName);
-    }
-
-    @Test
     public void createPodsIndexWithMinimumRequiredParams() {
         String podIndexName = RandomStringBuilder.build("create-pod-with-min", 8);
         Integer dimension = 3;
@@ -108,101 +83,4 @@ public class CreateDescribeListAndDeleteIndexTest {
         });
     }
 
-    @Test
-    public void createIndexWithInvalidName() {
-        CreateIndexRequestSpecPod podSpec = new CreateIndexRequestSpecPod().environment(environment).podType(indexPodType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name("Invalid-name")
-                .metric(IndexMetric.COSINE)
-                .dimension(10)
-                .spec(createIndexRequestSpec);
-
-        try {
-            controlPlaneClient.createIndex(createIndexRequest);
-
-            fail("Expected to throw PineconeBadRequestException");
-        } catch (PineconeBadRequestException expected) {
-            assertTrue(expected.getLocalizedMessage().contains("Name must consist of lower case alphanumeric characters or '-'"));
-        }
-    }
-
-    @Test
-    public void createIndexWithInvalidDimension() {
-        CreateIndexRequestSpecPod podSpec = new CreateIndexRequestSpecPod().environment(environment).podType(indexPodType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name("invalid-dimension")
-                .metric(IndexMetric.COSINE)
-                .dimension(-1)
-                .spec(createIndexRequestSpec);
-
-        try {
-            controlPlaneClient.createIndex(createIndexRequest);
-
-            fail("Expected to throw PineconeUnmappedHttpException");
-        } catch (PineconeUnmappedHttpException expected) {
-            assertTrue(expected.getLocalizedMessage().contains("dimension: invalid value: integer `-1`, expected u32"));
-        }
-    }
-
-    @Test
-    public void createIndexWithInvalidPods() {
-        CreateIndexRequestSpecPod podSpec =
-                new CreateIndexRequestSpecPod().environment(environment).pods(-1).podType(indexPodType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name("invalid-pods")
-                .metric(IndexMetric.COSINE)
-                .dimension(10)
-                .spec(createIndexRequestSpec);
-
-        try {
-            controlPlaneClient.createIndex(createIndexRequest);
-
-            fail("Expected to throw PineconeBadRequestException");
-        } catch (PineconeBadRequestException expected) {
-            assertTrue(expected.getLocalizedMessage().contains("Invalid value for pods: must be greater than 0"));
-        }
-    }
-
-    @Test
-    public void createIndexWithInvalidReplicas() {
-        CreateIndexRequestSpecPod podSpec =
-                new CreateIndexRequestSpecPod().environment(environment).pods(1).replicas(-1).podType(indexPodType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name("invalid-replicas")
-                .metric(IndexMetric.COSINE)
-                .dimension(10)
-                .spec(createIndexRequestSpec);
-
-        try {
-            controlPlaneClient.createIndex(createIndexRequest);
-
-            fail("Expected to throw PineconeBadRequestException");
-        } catch (PineconeBadRequestException expected) {
-            assertTrue(expected.getLocalizedMessage().contains("Invalid value for replicas: must be greater than 0"));
-        }
-    }
-
-    @Test
-    public void createIndexWithInvalidPodsToShards() {
-        CreateIndexRequestSpecPod podSpec =
-                new CreateIndexRequestSpecPod().environment(environment).pods(5).replicas(2).shards(2).podType(indexPodType);
-        CreateIndexRequestSpec createIndexRequestSpec = new CreateIndexRequestSpec().pod(podSpec);
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest()
-                .name("invalid-shards")
-                .metric(IndexMetric.COSINE)
-                .dimension(10)
-                .spec(createIndexRequestSpec);
-
-        try {
-            controlPlaneClient.createIndex(createIndexRequest);
-
-            fail("Expected to throw PineconeBadRequestException");
-        } catch (PineconeBadRequestException expected) {
-            assertTrue(expected.getLocalizedMessage().contains("Invalid value for pods: total pods must be divisible by number of shards"));
-        }
-    }
 }
