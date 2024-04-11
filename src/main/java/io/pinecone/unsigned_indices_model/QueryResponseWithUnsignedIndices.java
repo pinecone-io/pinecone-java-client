65a13865bc5f502f42a4299f0c447201ce3d0979
Rohan Shah <rohan.s@pinecone.io>
2024-04-11 15:39:07 -0400

    Add doc-strings for unsigned indices model classes (#108)
    ## Problem
    
    The java sdk contains custom model classes created to support the
    conversion of unsigned 32 bit integers to signed 32 bit integers and
    vice-versa, which are currently missing the doc-strings.
    
    ## Solution
    
    Add doc strings to the following model classes:
    1. QueryResponseWithUnsignedIndices
    2. ScoredVectorWithUnsignedIndices
    3. SparseValuesWithUnsignedIndices
    4. VectorWithUnsignedIndices
    
    ## Type of Change
    
    - [X] Non-code change (docs, etc)
    
    ## Test Plan
    
    CI tests

--- src/main/java/io/pinecone/unsigned_indices_model/QueryResponseWithUnsignedIndices.java
+++ src/main/java/io/pinecone/unsigned_indices_model/QueryResponseWithUnsignedIndices.java
@@ -8,13 +8,43 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class represents the response to a query, where the {@link ScoredVector} objects in the `matches` list
+ * contain {@link SparseValuesWithUnsignedIndices} instead of the standard SparseValues.
+ * <p>
+ * The {@link QueryResponseWithUnsignedIndices} class contains the following fields:
+ * - matches: a list of {@link ScoredVectorWithUnsignedIndices} objects, representing the matching vectors
+ * - namespace: the namespace of the index to retrieve search results from
+ * - usage: the usage information for the queryS
+ * <p>
+ * The class provides a constructor that takes a {@link QueryResponse} object and converts it to a
+ * {@link QueryResponseWithUnsignedIndices} object, as well as getter and setter methods for each of the fields.
+ * It also includes a method to convert a list of `ScoredVector` objects to a list of
+ * {@link ScoredVectorWithUnsignedIndices} objects.
+ */
 public class QueryResponseWithUnsignedIndices {
 
+    /**
+     * The list of matching vectors, where each vector contains sparse values with unsigned 32-bit integer indices.
+     */
     private List<ScoredVectorWithUnsignedIndices> matches;
+
+    /**
+     * The namespace of the query.
+     */
     private String namespace;
+
+    /**
+     * The usage information for the query.
+     */
     private Usage usage;
 
-
+    /**
+     * Constructs a {@link QueryResponseWithUnsignedIndices} object from a {@link QueryResponse} object, converting the
+     * `ScoredVector` objects to {@link ScoredVectorWithUnsignedIndices} objects.
+     *
+     * @param queryResponse The {@link QueryResponse} object to convert
+     */
     public QueryResponseWithUnsignedIndices(QueryResponse queryResponse) {
         if (queryResponse == null) {
             this.matches = Collections.emptyList();
@@ -27,14 +57,32 @@ public class QueryResponseWithUnsignedIndices {
         }
     }
 
+    /**
+     * Returns the list of matching vectors, where each vector contains sparse values with unsigned 32-bit integer indices.
+     *
+     * @return The list of {@link ScoredVectorWithUnsignedIndices} objects
+     */
     public List<ScoredVectorWithUnsignedIndices> getMatchesList() {
         return matches;
     }
 
+    /**
+     * Returns the {@link ScoredVectorWithUnsignedIndices} object at the specified index in the `matches` list.
+     *
+     * @param index The index of the {@link ScoredVectorWithUnsignedIndices} object to return
+     * @return The {@link ScoredVectorWithUnsignedIndices} object at the specified index
+     */
     public ScoredVectorWithUnsignedIndices getMatches(int index) {
         return matches.get(index);
     }
 
+    /**
+     * Converts a list of {@link ScoredVector} objects to a list of {@link ScoredVectorWithUnsignedIndices} objects.
+     *
+     * @param matches The list of {@link ScoredVector} objects to convert
+     * @return The list of {@link ScoredVectorWithUnsignedIndices} objects
+     * @throws IllegalArgumentException If the matches list is null
+     */
     public List<ScoredVectorWithUnsignedIndices> convertToScoredVectorWithUnsignedIndices(List<ScoredVector> matches) {
         if (matches == null) {
             throw new IllegalArgumentException("Matches list cannot be null.");
@@ -46,25 +94,56 @@ public class QueryResponseWithUnsignedIndices {
         return scoredVectorList;
     }
 
+    /**
+     * Sets the list of matching vectors, where each vector contains sparse values with unsigned 32-bit integer indices.
+     *
+     * @param matches The new list of {@link ScoredVectorWithUnsignedIndices} objects
+     */
     public void setMatches(List<ScoredVectorWithUnsignedIndices> matches) {
         this.matches = matches;
     }
 
+    /**
+     * Returns the namespace of the query.
+     *
+     * @return The namespace
+     */
     public String getNamespace() {
         return namespace;
     }
 
+    /**
+     * Sets the namespace of the query.
+     *
+     * @param namespace The new namespace
+     */
     public void setNamespace(String namespace) {
         this.namespace = namespace;
     }
 
-    public Usage getUsage() { return usage; }
-
-    public void setUsage(Usage usage) { this.usage = usage; }
+    /**
+     * Returns the usage information for the query.
+     *
+     * @return The usage
+     */
+    public Usage getUsage() {
+        return usage;
+    }
 
     /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
+     * Sets the usage information for the query.
+     *
+     * @param usage The new usage
+     */
+    public void setUsage(Usage usage) {
+        this.usage = usage;
+    }
+
+    /**
+     * Converts the given object to a string with each line indented by 4 spaces (except the first line).
+     *
+     * @param o The object to convert to a string
+     * @return The indented string representation of the object
      */
     private String toIndentedString(Object o) {
         if (o == null) {
--- src/main/java/io/pinecone/unsigned_indices_model/ScoredVectorWithUnsignedIndices.java
+++ src/main/java/io/pinecone/unsigned_indices_model/ScoredVectorWithUnsignedIndices.java
@@ -6,14 +6,55 @@ import io.pinecone.proto.ScoredVector;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class represents a scored vector with unsigned 32-bit integer indices for the sparse values.
+ * Unlike the {@link ScoredVector} class, which uses the SparseValues class to represent the sparse values,
+ * this class uses the {@link SparseValuesWithUnsignedIndices} class, which allows for the use of unsigned 32-bit
+ * integers as indices.
+ * <p>
+ * The {@link ScoredVectorWithUnsignedIndices} class contains the following fields:
+ * - score: the score associated with the vector
+ * - id: the identifier of the vector
+ * - values: the dense vector values
+ * - metadata: the metadata associated with the vector
+ * - sparseValuesWithUnsignedIndices: the sparse values associated with the vector, using unsigned 32-bit integer indices
+ * <p>
+ * The class provides a constructor that takes a {@link ScoredVector} object and converts it to a
+ * {@link ScoredVectorWithUnsignedIndices} object, as well as getter and setter methods for each of the fields.
+ */
 public class ScoredVectorWithUnsignedIndices {
 
+    /**
+     * The score associated with the vector.
+     */
     private float score;
+
+    /**
+     * The identifier of the vector.
+     */
     private String id;
+
+    /**
+     * The dense vector values.
+     */
     private List<Float> values;
+
+    /**
+     * The metadata associated with the vector.
+     */
     private Struct metadata;
+
+    /**
+     * The sparse values associated with the vector, using unsigned 32-bit integer indices.
+     */
     private SparseValuesWithUnsignedIndices sparseValuesWithUnsignedIndices;
 
+    /**
+     * Constructs a {@link ScoredVectorWithUnsignedIndices} object from a {@link ScoredVector} object, converting the
+     * SparseValues to {@link SparseValuesWithUnsignedIndices}.
+     *
+     * @param scoredVector the {@link ScoredVector} object to convert
+     */
     public ScoredVectorWithUnsignedIndices(ScoredVector scoredVector) {
         if (scoredVector == null) {
             this.score = 0F;
@@ -30,49 +71,101 @@ public class ScoredVectorWithUnsignedIndices {
         }
     }
 
+    /**
+     * Returns the score associated with the vector.
+     *
+     * @return The score
+     */
     public float getScore() {
         return score;
     }
 
+    /**
+     * Sets the score associated with the vector.
+     *
+     * @param score The new score
+     */
     public void setScore(float score) {
         this.score = score;
     }
 
+    /**
+     * Returns the identifier of the vector.
+     *
+     * @return The id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets the identifier of the vector.
+     *
+     * @param id The new id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Returns the dense vector values.
+     *
+     * @return The list of values
+     */
     public List<Float> getValuesList() {
         return values;
     }
 
+    /**
+     * Sets the dense vector values.
+     *
+     * @param values The new list of values
+     */
     public void setValues(List<Float> values) {
         this.values = values;
     }
 
+    /**
+     * Returns the metadata associated with the vector.
+     *
+     * @return The metadata
+     */
     public Struct getMetadata() {
         return metadata;
     }
 
+    /**
+     * Sets the metadata associated with the vector.
+     *
+     * @param metadata The new metadata
+     */
     public void setMetadata(Struct metadata) {
         this.metadata = metadata;
     }
 
+    /**
+     * Returns the sparse values associated with the vector, using unsigned 32-bit integer indices.
+     *
+     * @return The {@link SparseValuesWithUnsignedIndices} object
+     */
     public SparseValuesWithUnsignedIndices getSparseValuesWithUnsignedIndices() {
         return sparseValuesWithUnsignedIndices;
     }
 
+    /**
+     * Sets the sparse values associated with the vector, using unsigned 32-bit integer indices.
+     *
+     * @param sparseValuesWithUnsignedIndices The new {@link SparseValuesWithUnsignedIndices} object
+     */
     public void setSparseValuesWithUnsignedIndices(SparseValuesWithUnsignedIndices sparseValuesWithUnsignedIndices) {
         this.sparseValuesWithUnsignedIndices = sparseValuesWithUnsignedIndices;
     }
 
     /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
+     * Converts the given object to a string with each line indented by 4 spaces (except the first line).
+     *
+     * @param o The object to convert to a string
+     * @return The indented string representation of the object
      */
     private String toIndentedString(Object o) {
         if (o == null) {
