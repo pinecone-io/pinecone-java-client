c98c63e406380ae7bd5965e2537f41cc43f62d07
aulorbe <aulorbe@gmail.com>
2024-04-17 14:12:33 -0700

    Incorporate Austin feedback ListEndpointTest

--- src/integration/java/io/pinecone/integration/dataPlane/ListEndpointTest.java
+++ src/integration/java/io/pinecone/integration/dataPlane/ListEndpointTest.java
@@ -4,7 +4,6 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.pinecone.clients.AsyncIndex;
 import io.pinecone.clients.Index;
-import io.pinecone.clients.Pinecone;
 import io.pinecone.helpers.TestIndexResourcesManager;
 import io.pinecone.proto.ListResponse;
 import org.junit.jupiter.api.AfterAll;
@@ -15,64 +14,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ListEndpointTest {
     private static final TestIndexResourcesManager indexManager = TestIndexResourcesManager.getInstance();
-    private static String indexName ;
     private static Index indexConnection;
     private static AsyncIndex asyncIndexConnection;
 
     @BeforeAll
     public static void setUp() throws InterruptedException {
-        indexName = indexManager.getServerlessIndexName();
+        indexManager.getServerlessIndexName(); // create serverless index
         indexConnection = indexManager.getServerlessIndexConnection();
         asyncIndexConnection = indexManager.getServerlessAsyncIndexConnection();
     }
 
     @AfterAll
     public static void cleanUp() {
-        String apiKey = System.getenv("PINECONE_API_KEY");
-        Pinecone pinecone = new Pinecone.Builder(apiKey).build();
-        pinecone.deleteIndex(indexName);
         indexConnection.close();
         asyncIndexConnection.close();
     }
 
     @Test
-    public void testListEndpoint() throws InterruptedException {
-        Thread.sleep(10000); // wait for the index to be ready for operations
-
-        // Sync
+    public void testSyncListEndpoint() throws InterruptedException {
+        // Confirm all vector IDs from namespace are returned
         String listResponse = indexConnection.list("example-namespace").toString();
         assertTrue(listResponse.contains("id1"));
         assertTrue(listResponse.contains("id2"));
-        assertTrue(listResponse.contains("cidddd3"));
+        assertTrue(listResponse.contains("prefix-id3"));
 
+        // Confirm all vector IDs from namespace are returned
         String listResponseWithPrefix = indexConnection.list("example-namespace", "i").toString();
         assertTrue(listResponseWithPrefix.contains("id1"));
         assertTrue(listResponseWithPrefix.contains("id2"));
-        assertFalse(listResponseWithPrefix.contains("cidddd3")); // should not be in response
+        assertFalse(listResponseWithPrefix.contains("prefix-id3")); // should not be in response
 
-        String listResponseWithLimit = indexConnection.list("example-namespace", 1).toString();
-        assertTrue(listResponseWithLimit.contains("cidddd3")); // (IDs returned in LIFO order)
-        assertFalse(listResponseWithLimit.contains("id2")); // should not be in response
-        assertFalse(listResponseWithLimit.contains("id1")); // should not be in response
+        // Confirm all vector IDs from namespace are returned, with a limit of 1
+        ListResponse listResponseWithLimit = indexConnection.list("example-namespace", 1);
+        assertEquals(1, listResponseWithLimit.getVectorsList().size());
+    }
 
-        //Async
+    @Test
+    public void testAsyncListEndpoint() throws InterruptedException {
+        // Confirm all vector IDs from namespace are returned
         ListenableFuture<ListResponse> futureResponse = asyncIndexConnection.list("example-namespace");
         ListResponse asyncListResponse = Futures.getUnchecked(futureResponse);
         assertTrue(asyncListResponse.toString().contains("id1"));
         assertTrue(asyncListResponse.toString().contains("id2"));
-        assertTrue(asyncListResponse.toString().contains("cidddd3"));
+        assertTrue(asyncListResponse.toString().contains("prefix-id3"));
 
+        // Confirm all vector IDs from namespace are returned, filtered by given prefix
         ListenableFuture<ListResponse> futureResponseWithPrefix = asyncIndexConnection.list("example-namespace", "i");
         ListResponse asyncListResponseWithPrefix = Futures.getUnchecked(futureResponseWithPrefix);
         assertTrue(asyncListResponseWithPrefix.toString().contains("id1"));
         assertTrue(asyncListResponseWithPrefix.toString().contains("id2"));
-        assertFalse(asyncListResponseWithPrefix.toString().contains("cidddd3"));
+        assertFalse(asyncListResponseWithPrefix.toString().contains("prefix-id3"));
 
+        // Confirm all vector IDs from namespace are returned, with a limit of 1
         ListenableFuture<ListResponse> futureResponseWithLimit = asyncIndexConnection.list("example-namespace", 1);
         ListResponse asyncListResponseWithLimit = Futures.getUnchecked(futureResponseWithLimit);
-        assertTrue(asyncListResponseWithLimit.toString().contains("cidddd3"));
-        assertFalse(asyncListResponseWithLimit.toString().contains("id2"));
-        assertFalse(asyncListResponseWithLimit.toString().contains("id1"));
+        assertEquals(1, asyncListResponseWithLimit.getVectorsList().size());
     }
 
 }
