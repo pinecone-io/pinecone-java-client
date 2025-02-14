4f696e89f77932f2e7639c514cad90b1c0ad92dd
rohanshah18 <rohan.s@pinecone.io>
2025-02-14 11:25:30 -0500

    prepare to release v4.0.1

--- src/main/java/io/pinecone/commons/Constants.java
+++ src/main/java/io/pinecone/commons/Constants.java
@@ -1,5 +1,5 @@
 package io.pinecone.commons;
 
 public class Constants {
-    public static final String pineconeClientVersion = "v4.0.0";
+    public static final String pineconeClientVersion = "v4.0.1";
 }
