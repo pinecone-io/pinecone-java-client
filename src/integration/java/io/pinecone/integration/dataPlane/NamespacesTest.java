9b62df93ff092e63f0960a5bf7e18c967095be02
rohanshah18 <rohan.s@pinecone.io>
2025-05-08 13:32:03 -0400

    clean up namespaces test

--- src/integration/java/io/pinecone/integration/dataPlane/NamespacesTest.java
+++ src/integration/java/io/pinecone/integration/dataPlane/NamespacesTest.java
@@ -2,20 +2,22 @@ package io.pinecone.integration.dataPlane;
 
 import io.pinecone.clients.AsyncIndex;
 import io.pinecone.clients.Index;
+import io.pinecone.helpers.RandomStringBuilder;
 import io.pinecone.helpers.TestResourcesManager;
 import io.pinecone.proto.ListNamespacesResponse;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
+import static io.pinecone.helpers.BuildUpsertRequest.generateVectorValuesByDimension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NamespacesTest {
     private static final TestResourcesManager indexManager = TestResourcesManager.getInstance();
     private static Index index;
     private static AsyncIndex asyncIndex;
+    private static final int dimension = indexManager.getDimension();
 
     @AfterAll
     public static void cleanUp() {
@@ -24,56 +26,54 @@ public class NamespacesTest {
     }
 
     @Test
-    public void testNamespaces() throws InterruptedException {
+    public void namespacesSyncTest() throws InterruptedException {
+        String[] namespaces = new String[3];
         index = indexManager.getOrCreateServerlessIndexConnection();
         ListNamespacesResponse listNamespacesResponse = index.listNamespaces();
         int namespaceCount = listNamespacesResponse.getNamespacesCount();
 
-        String namespace1 = "namespace-1";
-        String namespace2 = "namespace-2";
-        String namespace3 = "namespace-3";
-
-        index.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace1);
-        index.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace2);
-        index.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace3);
+        for(int i=0; i<3; i++) {
+            namespaces[i] = RandomStringBuilder.build("namespace-", 3);
+            index.upsert("v"+i, generateVectorValuesByDimension(dimension), namespaces[i]);
+        }
 
         // wait for vectors to be upserted
         Thread.sleep(5000);
         listNamespacesResponse = index.listNamespaces();
-        assert(listNamespacesResponse.getNamespacesCount() == namespaceCount + 3);
+        assertEquals(listNamespacesResponse.getNamespacesCount(), namespaceCount + 3);
 
-        index.describeNamespace("namespace-1");
-        index.deleteNamespace("namespace-1");
+        index.describeNamespace(namespaces[0]);
+        index.deleteNamespace(namespaces[0]);
+
+        // wait for namespace to be deleted
+        Thread.sleep(3000);
         listNamespacesResponse = index.listNamespaces();
         assertEquals(listNamespacesResponse.getNamespacesCount(), namespaceCount + 2);
     }
 
     @Test
-    public void testNamespacesAsync() throws InterruptedException, ExecutionException {
+    public void namespacesAsyncTest() throws InterruptedException, ExecutionException {
+        String[] namespaces = new String[3];
         asyncIndex = indexManager.getOrCreateServerlessAsyncIndexConnection();
 
         ListNamespacesResponse listNamespacesResponse = asyncIndex.listNamespaces().get();
         int namespaceCount = listNamespacesResponse.getNamespacesCount();
 
-        String namespace1 = "namespace-1";
-        String namespace2 = "namespace-2";
-        String namespace3 = "namespace-3";
-
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace1);
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace2);
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace3);
-
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace1);
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace2);
-        asyncIndex.upsert("v1", Arrays.asList(1F, 2F, 3F), namespace3);
+        for(int i=0; i<3; i++) {
+            namespaces[i] = RandomStringBuilder.build("namespace-", 3);
+            asyncIndex.upsert("v"+i, generateVectorValuesByDimension(dimension), namespaces[i]);
+        }
 
         // wait for vectors to be upserted
         Thread.sleep(5000);
         listNamespacesResponse = asyncIndex.listNamespaces().get();
-        assert(listNamespacesResponse.getNamespacesCount() == namespaceCount + 3);
+        assertEquals(listNamespacesResponse.getNamespacesCount(), namespaceCount + 3);
 
-        asyncIndex.describeNamespace(namespace1);
-        asyncIndex.deleteNamespace(namespace1);
+        asyncIndex.describeNamespace(namespaces[0]);
+        asyncIndex.deleteNamespace(namespaces[0]);
+
+        // wait for namespace to be deleted
+        Thread.sleep(3000);
         listNamespacesResponse = asyncIndex.listNamespaces().get();
         assertEquals(listNamespacesResponse.getNamespacesCount(), namespaceCount + 2);
     }
