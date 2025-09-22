954081dfb7718751abfd5b6c428e291bf835042d
rohanshah18 <rohan.s@pinecone.io>
2025-09-22 14:46:29 -0400

    fix flaky tests

--- src/integration/java/io/pinecone/integration/controlPlane/serverless/DeletionProtectionTest.java
+++ src/integration/java/io/pinecone/integration/controlPlane/serverless/DeletionProtectionTest.java
@@ -17,39 +17,53 @@ public class DeletionProtectionTest {
             .build();
 
     @Test
-    public void createIndexWithDeletionProtectionEnabled() {
+    public void createIndexWithDeletionProtectionEnabled() throws InterruptedException {
         String indexName = RandomStringBuilder.build("create-serv", 8);
         HashMap<String, String> expectedTags = new HashMap<>();
         expectedTags.put("test", "deletion-protection-enabled");
         // Create serverless index with deletion protection enabled
         controlPlaneClient.createServerlessIndex(indexName, "cosine", 3, "aws", "us-west-2", DeletionProtection.ENABLED, expectedTags);
+
+        // Wait for index to be created
+        Thread.sleep(5000);
+
         // Describe index to verify deletion protection is enabled
         IndexModel indexModel = controlPlaneClient.describeIndex(indexName);
         DeletionProtection deletionProtection = indexModel.getDeletionProtection();
-        Assertions.assertEquals(deletionProtection, DeletionProtection.ENABLED);
+        Assertions.assertEquals(DeletionProtection.ENABLED, deletionProtection);
         Map<String, String> actualTags = indexModel.getTags();
         Assertions.assertEquals(expectedTags, actualTags);
     }
 
     @Test
-    public void createPodIndexWithDeletionProtectionDisabled() {
+    public void createIndexWithDeletionProtectionDisabled() throws InterruptedException {
         String indexName = RandomStringBuilder.build("create-pod", 8);
         HashMap<String, String> expectedTags = new HashMap<>();
         expectedTags.put("test", "deletion-protection-disabled");
         // Create serverless index with deletion protection disabled
         controlPlaneClient.createServerlessIndex(indexName, "cosine", 3, "aws", "us-west-2", DeletionProtection.DISABLED, expectedTags);
+
+        // Wait for index to be created
+        Thread.sleep(5000);
+
         IndexModel indexModel = controlPlaneClient.describeIndex(indexName);
         DeletionProtection deletionProtection = indexModel.getDeletionProtection();
-        Assertions.assertEquals(deletionProtection, DeletionProtection.DISABLED);
+        Assertions.assertEquals(DeletionProtection.DISABLED, deletionProtection);
         Map<String, String> actualTags = indexModel.getTags();
         Assertions.assertEquals(expectedTags, actualTags);
+
         // Configure index to enable deletionProtection
         controlPlaneClient.configureServerlessIndex(indexName, DeletionProtection.ENABLED, expectedTags, null);
         indexModel = controlPlaneClient.describeIndex(indexName);
         deletionProtection = indexModel.getDeletionProtection();
-        Assertions.assertEquals(deletionProtection, DeletionProtection.ENABLED);
+        Assertions.assertEquals(DeletionProtection.ENABLED, deletionProtection);
+
         // Configure index to disable deletionProtection
         controlPlaneClient.configureServerlessIndex(indexName, DeletionProtection.DISABLED, expectedTags, null);
+
+        // Wait for index to be configured
+        Thread.sleep(5000);
+
         // Delete index
         controlPlaneClient.deleteIndex(indexName);
     }
